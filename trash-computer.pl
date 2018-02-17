11652f2c28c1fed702e05afa9cd4ba512a4d6877
Gabriele Falace <gabrielefalace@gmail.com>
2018-02-17 08:11:04 +0100

    Update trash-computer.pl

--- trash-computer.pl
+++ trash-computer.pl
@@ -1,10 +1,12 @@
 /* example: ?- trash(myasus). */
 
+/* facts */
 broken(myasus, cpu).
 broken(myasus, motherboard).
 doesNotStart(myHp).
 hardDiskBurned(myMac).
 
+/* rules */
 trash(Computer) :- broken(Computer, motherboard).
 trash(Computer) :- broken(Computer, cpu).
 trash(Computer) :- doesNotStart(Computer), hardDiskBurned(Computer).
