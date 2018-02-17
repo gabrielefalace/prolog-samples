221732905d05fd8c5fa13741320b131baf53e88b
Gabriele Falace <gabrielefalace@gmail.com>
2018-02-17 08:07:47 +0100

    Create trash-computer.pl

---
+++ trash-computer.pl
@@ -0,0 +1,8 @@
+broken(myasus, cpu).
+broken(myasus, motherboard).
+doesNotStart(myHp).
+hardDiskBurned(myMac).
+
+trash(Computer) :- broken(Computer, motherboard).
+trash(Computer) :- broken(Computer, cpu).
+trash(Computer) :- doesNotStart(Computer), hardDiskBurned(Computer).
